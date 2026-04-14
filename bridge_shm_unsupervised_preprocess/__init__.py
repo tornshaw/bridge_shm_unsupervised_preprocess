22a0a3a42783fd18d0917bfe212fc2feef16462a
Xiao Tugang <39145280+tornshaw@users.noreply.github.com>
2026-04-14 13:33:12 +0800

    refactor: package v2 pipeline into bridge_shm_unsupervised_preprocess

---
+++ bridge_shm_unsupervised_preprocess/__init__.py
@@ -0,0 +1,5 @@
+"""bridge_shm_unsupervised_preprocess package."""
+
+from .core import BridgeSHMUnsupervisedPreprocessor, main
+
+__all__ = ["BridgeSHMUnsupervisedPreprocessor", "main"]
---
+++ bridge_shm_unsupervised_preprocess/cli.py
@@ -0,0 +1,7 @@
+"""CLI entrypoint for bridge_shm_unsupervised_preprocess."""
+
+from .core import main
+
+
+if __name__ == "__main__":
+    main()
